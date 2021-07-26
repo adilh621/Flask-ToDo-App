dbb1842fc7dc0f61bbc3a8a5e315173940484bc4
Adil Hussain <adilh621@gmail.com>
2021-07-26 19:21:01 -0400

    adding crud functions to app

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200))
     complete = db.Column(db.Boolean)
+    
 
     
 #routes
@@ -36,6 +37,23 @@ def complete(id):
     
     return redirect(url_for('index'))
 
+# @app.route("/delete", methods=['POST'])
+# def delete():
+#     text = request.form.get("text")
+#     todo = Todo.query.filter_by(text=text).first()
+#     db.session.delete(todo)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was an error while deleting that task'
+
+        
 if __name__ == '__main__':
     app.run(debug=True)
 
